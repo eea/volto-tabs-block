1551764862a37ae6afee5c35ee813239c5b49b75
Gorka Fernandez <gfernandez@codesyntax.eus>
2022-11-29 14:34:15 +0100

    messages

--- src/components/layoutSchema.js
+++ src/components/layoutSchema.js
@@ -1,80 +1,157 @@
+import { defineMessages, useIntl } from "react-intl";
+
 const blocksArray = (config) => {
-  return Object.keys(config.blocks.blocksConfig)
-    .map((key) => {
-      if (config.blocks.blocksConfig[key]?.restricted) {
-        return false;
-      } else {
-        const title = config.blocks.blocksConfig[key]?.title || key;
-        return [key, title];
-      }
-    })
-    .filter((val) => !!val);
+	return Object.keys(config.blocks.blocksConfig)
+		.map((key) => {
+			if (config.blocks.blocksConfig[key]?.restricted) {
+				return false;
+			} else {
+				const title = config.blocks.blocksConfig[key]?.title || key;
+				return [key, title];
+			}
+		})
+		.filter((val) => !!val);
 };
+const intl = useIntl();
+const messages = defineMessages({
+	tabsBlockSetting: {
+		id: "tabsBlockSetting",
+		defaultMessage: "Tabs block setting",
+	},
+	default: {
+		id: "default",
+		defaultMessage: "Default",
+	},
+	allowedBlocks: {
+		id: "allowedBlocks",
+		defaultMessage: "Allowed blocks",
+	},
+	allowedBlocksDescription: {
+		id: "allowedBlocksDescription",
+		defaultMessage: "Allow only the following block types",
+	},
+	helperText: {
+		id: "helperText",
+		defaultMessage: "Helper text",
+	},
+	placeholderDescription: {
+		id: "placeholderDescription",
+		defaultMessage:
+			"A short hint that describes the expected value within this block",
+	},
+	instructions: {
+		id: "instructions",
+		defaultMessage: "Instructions",
+	},
+	instructionsDescription: {
+		id: "instructionsDescription",
+		defaultMessage: "Detailed expected value within this block",
+	},
+	required: {
+		id: "required",
+		defaultMessage: "Required",
+	},
+	requiredDescription: {
+		id: "requiredDescription",
+		defaultMessage: "Don't allow deletion of this block",
+	},
+	fixed: {
+		id: "fixed",
+		defaultMessage: "Fixed position",
+	},
+	fixedDescription: {
+		id: "fixedDescription",
+		defaultMessage: "Disable drag & drop on this block",
+	},
+	disableNewBlocks: {
+		id: "disableNewBlocks",
+		defaultMessage: "Disable new blocks",
+	},
+	disableNewBlocksDescription: {
+		id: "instructionsDescription",
+		defaultMessage: "Disable creation of new blocks after this block",
+	},
+	readOnly: {
+		id: "readOnly",
+		defaultMessage: "Read-only",
+	},
+	readOnlyDescription: {
+		id: "readOlnyDescription",
+		defaultMessage: "Disable editing on this block",
+	},
+	readOnlySettings: {
+		id: "readOnlySettings",
+		defaultMessage: "Read-only settings",
+	},
+	readOnlySettingsDescription: {
+		id: "readOnlySettingsDescription",
+		defaultMessage: "Disable editing on columns block settings",
+	},
+});
 
 export default (config) => ({
-  title: 'Tabs block settings',
-  fieldsets: [
-    {
-      id: 'default',
-      title: 'Default',
-      fields: [
-        'placeholder',
-        'instructions',
-        'allowedBlocks',
-        'required',
-        'fixed',
-        'disableNewBlocks',
-        'readOnly',
-        'readOnlySettings',
-      ],
-    },
-  ],
-  properties: {
-    allowedBlocks: {
-      title: 'Allowed blocks',
-      description: 'Allow only the following blocks types',
-      type: 'array',
-      items: {
-        choices: blocksArray(config),
-      },
-    },
-    placeholder: {
-      title: 'Helper text',
-      description:
-        'A short hint that describes the expected value within this block',
-      type: 'string',
-    },
-    instructions: {
-      title: 'Instructions',
-      description: 'Detailed expected value within this block',
-      type: 'string',
-      widget: 'richtext',
-    },
-    required: {
-      title: 'Required',
-      description: "Don't allow deletion of this block",
-      type: 'boolean',
-    },
-    fixed: {
-      title: 'Fixed position',
-      description: 'Disable drag & drop on this block',
-      type: 'boolean',
-    },
-    disableNewBlocks: {
-      title: 'Disable new blocks',
-      description: 'Disable creation of new blocks after this block',
-      type: 'boolean',
-    },
-    readOnly: {
-      title: 'Read-only',
-      description: 'Disable editing on this block',
-      type: 'boolean',
-    },
-    readOnlySettings: {
-      title: 'Read-only settings',
-      description: 'Disable editing on columns block settings',
-      type: 'boolean',
-    },
-  },
-  required: [],
+	title: intl.formatMessage(messages.tabsBlockSetting),
+	fieldsets: [
+		{
+			id: "default",
+			title: intl.formatMessage(messages.default),
+			fields: [
+				"placeholder",
+				"instructions",
+				"allowedBlocks",
+				"required",
+				"fixed",
+				"disableNewBlocks",
+				"readOnly",
+				"readOnlySettings",
+			],
+		},
+	],
+	properties: {
+		allowedBlocks: {
+			title: intl.formatMessage(messages.allowedBlocks),
+			description: intl.formatMessage(messages.allowedBlocksDescription),
+			type: "array",
+			items: {
+				choices: blocksArray(config),
+			},
+		},
+		placeholder: {
+			title: intl.formatMessage(messages.helperText),
+			description: intl.formatMessage(messages.placeholderDescription),
+			type: "string",
+		},
+		instructions: {
+			title: intl.formatMessage(messages.instructions),
+			description: intl.formatMessage(messages.instructionsDescription),
+			type: "string",
+			widget: "richtext",
+		},
+		required: {
+			title: intl.formatMessage(messages.required),
+			description: intl.formatMessage(messages.requiredDescription),
+			type: "boolean",
+		},
+		fixed: {
+			title: intl.formatMessage(messages.fixed),
+			description: intl.formatMessage(messages.fixedDescription),
+			type: "boolean",
+		},
+		disableNewBlocks: {
+			title: disableNewBlocks,
+			description: intl.formatMessage(messages.disableNewBlocksDescription),
+			type: "boolean",
+		},
+		readOnly: {
+			title: intl.formatMessage(messages.readOnly),
+			description: intl.formatMessage(messages.readOnlyDescription),
+			type: "boolean",
+		},
+		readOnlySettings: {
+			title: intl.formatMessage(messages.readOnlySettings),
+			description: intl.formatMessage(messages.readOnlySettingsDescription),
+			type: "boolean",
+		},
+	},
+	required: [],
 });
--- src/components/schema.js
+++ src/components/schema.js
@@ -1,60 +1,88 @@
-import { TABS_BLOCK } from '@eeacms/volto-tabs-block/constants';
+import { TABS_BLOCK } from "@eeacms/volto-tabs-block/constants";
+import { defineMessages, useIntl } from "react-intl";
 
 export const schema = (config, templateSchema = {}) => {
-  const templatesConfig = config.blocks.blocksConfig[TABS_BLOCK].templates;
-  const templates = Object.keys(templatesConfig).map((template) => [
-    template,
-    templatesConfig[template].title || template,
-  ]);
+	const templatesConfig = config.blocks.blocksConfig[TABS_BLOCK].templates;
+	const templates = Object.keys(templatesConfig).map((template) => [
+		template,
+		templatesConfig[template].title || template,
+	]);
 
-  const defaultFieldset = templateSchema?.fieldsets?.filter(
-    (fieldset) => fieldset.id === 'default',
-  )[0];
+	const defaultFieldset = templateSchema?.fieldsets?.filter(
+		(fieldset) => fieldset.id === "default"
+	)[0];
+	const intl = useIntl();
+	const messages = defineMessages({
+		tabsBlock: {
+			id: "tabsBlock",
+			defaultMessage: "Tabs Block",
+		},
+		default: {
+			id: "default",
+			defaultMessage: "Default",
+		},
+		title: {
+			id: "title",
+			defaultMessage: "Title",
+		},
+		tabs: {
+			id: "tabs",
+			defaultMessage: "Tabs",
+		},
+		template: {
+			id: "template",
+			defaultMessage: "Template",
+		},
+		verticalAlign: {
+			id: "verticalAlign",
+			defaultMessage: "Vertical Align",
+		},
+	});
 
-  return {
-    title: templateSchema?.title || 'Tabs block',
-    fieldsets: [
-      {
-        id: 'default',
-        title: 'Default',
-        fields: [
-          'data',
-          'title',
-          'template',
-          'verticalAlign',
-          ...(defaultFieldset?.fields || []),
-        ],
-      },
-      ...(templateSchema?.fieldsets?.filter(
-        (fieldset) => fieldset.id !== 'default',
-      ) || []),
-    ],
-    properties: {
-      data: {
-        title: 'Tabs',
-        type: 'tabs',
-      },
-      title: {
-        title: 'Title',
-      },
-      template: {
-        title: 'Template',
-        type: 'array',
-        choices: [...templates],
-        default: 'default',
-      },
-      verticalAlign: {
-        title: 'Vertical align',
-        type: 'array',
-        choices: [
-          ['flex-start', 'Top'],
-          ['center', 'Middle'],
-          ['flex-end', 'Bottom'],
-        ],
-        default: 'flex-start',
-      },
-      ...(templateSchema?.properties || {}),
-    },
-    required: [...(templateSchema?.required || [])],
-  };
+	return {
+		title: templateSchema?.title || intl.formatMessage(messages.tabsBlock),
+		fieldsets: [
+			{
+				id: "default",
+				title: "Default",
+				fields: [
+					"data",
+					"title",
+					"template",
+					"verticalAlign",
+					...(defaultFieldset?.fields || []),
+				],
+			},
+			...(templateSchema?.fieldsets?.filter(
+				(fieldset) => fieldset.id !== "default"
+			) || []),
+		],
+		properties: {
+			data: {
+				title: intl.formatMessage(messages.tabs),
+				type: "tabs",
+			},
+			title: {
+				title: intl.formatMessage(messages.title),
+			},
+			template: {
+				title: intl.formatMessage(messages.template),
+				type: "array",
+				choices: [...templates],
+				default: "default",
+			},
+			verticalAlign: {
+				title: intl.formatMessage(messages.verticalAlign),
+				type: "array",
+				choices: [
+					["flex-start", "Top"],
+					["center", "Middle"],
+					["flex-end", "Bottom"],
+				],
+				default: "flex-start",
+			},
+			...(templateSchema?.properties || {}),
+		},
+		required: [...(templateSchema?.required || [])],
+	};
 };
