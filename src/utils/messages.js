d4480c13600d5ce8c85517d8733debe9f0f4d6bc
Gorka Fernandez <gfernandez@codesyntax.eus>
2022-12-01 09:32:55 +0100

    Create messages.js
    -File to manage strings

---
+++ src/utils/messages.js
@@ -0,0 +1,309 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  tabsBlockSetting: {
+    id: 'tabsBlockSetting',
+    defaultMessage: 'Tabs block setting',
+  },
+  default: {
+    id: 'default',
+    defaultMessage: 'Default',
+  },
+  allowedBlocks: {
+    id: 'allowedBlocks',
+    defaultMessage: 'Allowed blocks',
+  },
+  allowedBlocksDescription: {
+    id: 'allowedBlocksDescription',
+    defaultMessage: 'Allow only the following block types',
+  },
+  helperText: {
+    id: 'helperText',
+    defaultMessage: 'Helper text',
+  },
+  placeholderDescription: {
+    id: 'placeholderDescription',
+    defaultMessage:
+      'A short hint that describes the expected value within this block',
+  },
+  instructions: {
+    id: 'instructions',
+    defaultMessage: 'Instructions',
+  },
+  instructionsDescription: {
+    id: 'instructionsDescription',
+    defaultMessage: 'Detailed expected value within this block',
+  },
+  required: {
+    id: 'required',
+    defaultMessage: 'Required',
+  },
+  requiredDescription: {
+    id: 'requiredDescription',
+    defaultMessage: "Don't allow deletion of this block",
+  },
+  fixed: {
+    id: 'fixed',
+    defaultMessage: 'Fixed position',
+  },
+  fixedDescription: {
+    id: 'fixedDescription',
+    defaultMessage: 'Disable drag & drop on this block',
+  },
+  disableNewBlocks: {
+    id: 'disableNewBlocks',
+    defaultMessage: 'Disable new blocks',
+  },
+  disableNewBlocksDescription: {
+    id: 'disableNewBlocksDescription',
+    defaultMessage: 'Disable creation of new blocks after this block',
+  },
+  readOnly: {
+    id: 'readOnly',
+    defaultMessage: 'Read-only',
+  },
+  readOnlyDescription: {
+    id: 'readOlnyDescription',
+    defaultMessage: 'Disable editing on this block',
+  },
+  readOnlySettings: {
+    id: 'readOnlySettings',
+    defaultMessage: 'Read-only settings',
+  },
+  readOnlySettingsDescription: {
+    id: 'readOnlySettingsDescription',
+    defaultMessage: 'Disable editing on columns block settings',
+  },
+  tabsBlock: {
+    id: 'tabsBlock',
+    defaultMessage: 'Tabs Block',
+  },
+  title: {
+    id: 'title',
+    defaultMessage: 'Title',
+  },
+  tabs: {
+    id: 'tabs',
+    defaultMessage: 'Tabs',
+  },
+  template: {
+    id: 'template',
+    defaultMessage: 'Template',
+  },
+  verticalAlign: {
+    id: 'verticalAlign',
+    defaultMessage: 'Vertical Align',
+  },
+  top: {
+    id: 'top',
+    defaultMessage: 'Top',
+  },
+  middle: {
+    id: 'middle',
+    defaultMessage: 'Middle',
+  },
+  bottom: {
+    id: 'bottom',
+    defaultMessage: 'Bottom',
+  },
+  menu: {
+    id: 'menu',
+    defaultMessage: 'Menu',
+  },
+  description: {
+    id: 'description',
+    defaultMessage: 'Description',
+  },
+  position: {
+    id: 'position',
+    defaultMessage: 'Position',
+  },
+  fluid: {
+    id: 'fluid',
+    defaultMessage: 'Fluid',
+  },
+  aligment: {
+    id: 'aligment',
+    defaultMessage: 'Aligment',
+  },
+  size: {
+    id: 'size',
+    defaultMessage: 'Size',
+  },
+  color: {
+    id: 'color',
+    defaultMessage: 'Color',
+  },
+  borderless: {
+    id: 'borderless',
+    defaultMessage: 'Borderless',
+  },
+  compact: {
+    id: 'compact',
+    defaultMessage: 'Compact',
+  },
+  inverted: {
+    id: 'inverted',
+    defaultMessage: 'Inverted',
+  },
+  pointing: {
+    id: 'pointing',
+    defaultMessage: 'Pointing',
+  },
+  secondary: {
+    id: 'secondary',
+    defaultMessage: 'Secondary',
+  },
+  stackable: {
+    id: 'stackable',
+    defaultMessage: 'Stackable',
+  },
+  tabular: {
+    id: 'tabular',
+    defaultMessage: 'Tabular',
+  },
+  text: {
+    id: 'text',
+    defaultMessage: 'Text',
+  },
+  leftSide: {
+    id: 'leftSide',
+    defaultMessage: 'Left side',
+  },
+  rightSide: {
+    id: 'rightSide',
+    defaultMessage: 'Right side',
+  },
+  left: {
+    id: 'left',
+    defaultMessage: 'Left',
+  },
+  right: {
+    id: 'right',
+    defaultMessage: 'Right',
+  },
+  center: {
+    id: 'center',
+    defaultMessage: 'Center',
+  },
+  spaceBetween: {
+    id: 'spaceBetween',
+    defaultMessage: 'Space between',
+  },
+  mini: {
+    id: 'mini',
+    defaultMessage: 'Mini',
+  },
+  tiny: {
+    id: 'tiny',
+    defaultMessage: 'Tiny',
+  },
+  small: {
+    id: 'small',
+    defaultMessage: 'Small',
+  },
+  large: {
+    id: 'large',
+    defaultMessage: 'Large',
+  },
+  huge: {
+    id: 'huge',
+    defaultMessage: 'Huge',
+  },
+  massive: {
+    id: 'massive',
+    defaultMessage: 'Massive',
+  },
+  red: {
+    id: 'red',
+    defaultMessage: 'Red',
+  },
+  orange: {
+    id: 'orange',
+    defaultMessage: 'Orange',
+  },
+  yellow: {
+    id: 'yellow',
+    defaultMessage: 'Yellow',
+  },
+  olive: {
+    id: 'olive',
+    defaultMessage: 'Olive',
+  },
+  green: {
+    id: 'green',
+    defaultMessage: 'Green',
+  },
+  teal: {
+    id: 'teal',
+    defaultMessage: 'Teal',
+  },
+  blue: {
+    id: 'blue',
+    defaultMessage: 'Blue',
+  },
+  violet: {
+    id: 'violet',
+    defaultMessage: 'Violet',
+  },
+  purple: {
+    id: 'purple',
+    defaultMessage: 'Purple',
+  },
+  pink: {
+    id: 'pink',
+    defaultMessage: 'Pink',
+  },
+  brown: {
+    id: 'brown',
+    defaultMessage: 'Brown',
+  },
+  grey: {
+    id: 'grey',
+    defaultMessage: 'Grey',
+  },
+  black: {
+    id: 'black',
+    defaultMessage: 'Black',
+  },
+  horizontalTabsBlock: {
+    id: 'horizontalTabsBlock',
+    defaultMessage: 'Horizontal tabs block',
+  },
+  defaultTabsBlock: {
+    id: 'defaultTabsBlock',
+    defaultMessage: 'Default tabs block',
+  },
+  carouselBlock: {
+    id: 'carouselBlock',
+    defaultMessage: 'Carousel block',
+  },
+  theme: {
+    id: 'theme',
+    defaultMessage: 'Theme',
+  },
+  light: {
+    id: 'light',
+    defaultMessage: 'Light',
+  },
+  dark: {
+    id: 'dark',
+    defaultMessage: 'Dark',
+  },
+  selectSlide: {
+    id: 'selectSlide',
+    defaultMessage: 'Select slide',
+  },
+  acordionTabsBlock: {
+    id: 'acordionTabsBlock',
+    defaultMessage: 'Acordion tabs block',
+  },
+  unknownBlock: {
+    id: 'Unknown Block',
+    defaultMessage: 'Unknown Block {block}',
+  },
+  delete: {
+    id: 'delete',
+    defaultMessage: 'delete',
+  },
+});
