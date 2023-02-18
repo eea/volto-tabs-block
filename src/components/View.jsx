6a867ee43dc35198a5e6cf7ee7a733e2adbfff71
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-02-18 18:02:30 +0200

    moving tabs

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import cx from 'classnames';
 import { StyleWrapperView } from '@eeacms/volto-block-style/StyleWrapper';
 import { TABS_BLOCK } from '@eeacms/volto-tabs-block/constants';
 import { DefaultView } from './templates/default';
-
 import config from '@plone/volto/registry';
-
 import '@eeacms/volto-tabs-block/less/edit.less';
 import '@eeacms/volto-tabs-block/less/tabs.less';
 
 const View = (props) => {
   const view = React.useRef(null);
-  const { data = {}, uiContainer = '' } = props;
+  const { data = {}, uiContainer = '', blockProps } = props;
   const metadata = props.metadata || props.properties;
   const template = data.template || 'default';
   const tabsData = data.data || {};
@@ -22,11 +20,22 @@ const View = (props) => {
   const tabData = tabs[activeTab] || {};
   const theme = data.theme || 'light';
   const verticalAlign = data.verticalAlign || 'flex-start';
-
   const TabsView =
     config.blocks.blocksConfig[TABS_BLOCK].templates?.[template]?.view ||
     DefaultView;
-
+  const ref = useRef(null);
+  const handleKeyDownTabs = (event) => {
+    if (event.key === 'Enter') {
+    }
+    if (event.key === 'Tab') {
+      if (activeTabIndex === tabsList.length - 1) {
+        return;
+      } else {
+        setActiveTab(tabsList[(activeTabIndex + 1) % tabsList.length]);
+        event.preventDefault();
+      }
+    }
+  };
   return (
     <StyleWrapperView
       {...props}
@@ -37,13 +46,17 @@ const View = (props) => {
       <div
         className={cx('tabs-block', template, theme, verticalAlign)}
         id={props.id}
-        ref={view}
+        tabIndex="0"
+        onKeyDown={handleKeyDownTabs}
+        role="button"
       >
         <StyleWrapperView
           {...props}
           data={tabData}
           styleData={tabData.styles || {}}
           styled={true}
+          tabIndex="0"
+          ref={ref}
         >
           <TabsView
             {...props}
