6c305aae657c801c6e5e97db5747af56611602f2
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-05-03 16:04:31 +0300

    first

--- src/components/templates/accordion/Edit.jsx
+++ src/components/templates/accordion/Edit.jsx
@@ -1,18 +1,329 @@
-import { DefaultEdit } from '../default';
+import React, { useLayoutEffect } from 'react';
+import cx from 'classnames';
+import { compose } from 'redux';
+import { v4 as uuid } from 'uuid';
+import { withRouter } from 'react-router';
+import Tabs from 'react-responsive-tabs';
+import AnimateHeight from 'react-animate-height';
+import { Icon } from 'semantic-ui-react';
+import { Menu, Input, Container } from 'semantic-ui-react';
+import config from '@plone/volto/registry';
+import { Icon as VoltoIcon, RenderBlocks } from '@plone/volto/components';
+import { TABS_BLOCK } from '@eeacms/volto-tabs-block/constants';
+import { withScrollToTarget } from '@eeacms/volto-tabs-block/hocs';
+import { getParentTabFromHash } from '@eeacms/volto-tabs-block/helpers';
+import noop from 'lodash/noop';
+import {
+  SimpleMarkdown,
+  getMenuPosition,
+} from '@eeacms/volto-tabs-block/utils';
+import 'react-responsive-tabs/styles.css';
+import '@eeacms/volto-tabs-block/less/menu.less';
 
-const Edit = (props) => {
+const MenuItem = (props) => {
+  const inputRef = React.useRef(null);
+  const {
+    activeTab = null,
+    activeBlock = null,
+    block = null,
+    data = {},
+    editingTab = null,
+    selected = false,
+    tabData = {},
+    tabs = {},
+
+    tabsData = {},
+    tabsDescription,
+    tabsList = [],
+    tabsTitle,
+    emptyTab = noop,
+    setActiveBlock = noop,
+    setActiveTab = noop,
+    setEditingTab = noop,
+    onChangeBlock = noop,
+  } = props;
+  const { tab, index } = props;
+  const tabIndex = index + 1;
+  const defaultTitle = `Tab ${tabIndex}`;
+  const title = tabs[tab]?.title;
+  const addNewTab = () => {
+    const tabId = uuid();
+
+    onChangeBlock(block, {
+      ...data,
+      data: {
+        ...tabsData,
+        blocks: {
+          ...tabsData.blocks,
+          [tabId]: {
+            ...emptyTab(),
+          },
+        },
+        blocks_layout: {
+          items: [...tabsData.blocks_layout.items, tabId],
+        },
+      },
+    });
+  };
+
+  React.useEffect(() => {
+    if (editingTab === tab && inputRef.current) {
+      inputRef.current.focus();
+    }
+    /* eslint-disable-next-line */
+  }, [editingTab]);
+
   return (
-    <DefaultEdit
-      {...props}
-      data={{
-        ...props.data,
-        menuPointing: true,
-        menuSecondary: true,
-        menuFluid: false,
-        menuText: false,
-      }}
-    />
+    <>
+      {index === 0 && (tabsTitle || tabsDescription) && (
+        <div className="menu-title"></div>
+      )}
+      <MenuItem
+        role="tab"
+        aria-hidden="true"
+        name={defaultTitle}
+        active={tab === activeTab}
+        onClick={() => {
+          if (activeTab !== tab) {
+            setActiveTab(tab);
+          }
+          if (activeBlock) {
+            setActiveBlock(null);
+          }
+          if (editingTab !== tab) {
+            setEditingTab(null);
+          }
+        }}
+        onDoubleClick={() => {
+          setEditingTab(tab);
+        }}
+      >
+        {editingTab === tab && selected ? (
+          <Input
+            placeholder={defaultTitle}
+            ref={inputRef}
+            transparent
+            value={title}
+            onChange={(e) => {
+              onChangeBlock(block, {
+                ...data,
+                data: {
+                  ...tabsData,
+                  blocks: {
+                    ...tabsData.blocks,
+                    [tab]: {
+                      ...(tabData || {}),
+                      title: e.target.value,
+                    },
+                  },
+                },
+              });
+            }}
+          />
+        ) : (
+          <>
+            <p className={'menu-item-text'}>{title || defaultTitle}</p>
+          </>
+        )}
+      </MenuItem>
+      {index === tabsList.length - 1 ? (
+        <>
+          <MenuItem
+            role="tab"
+            aria-hidden="true"
+            name="addition"
+            onClick={() => {
+              addNewTab();
+              setEditingTab(null);
+            }}
+          >
+            <p
+              className={'menu-item-text'}
+              style={{ paddingLeft: '30px', borderStyle: 'none' }}
+            >
+              +
+            </p>
+          </MenuItem>
+        </>
+      ) : (
+        ''
+      )}
+    </>
   );
 };
 
-export default Edit;
+class Tab extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      height: 0,
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.height === 0) {
+      requestAnimationFrame(() => {
+        this.setState({ height: 'auto' });
+      });
+    }
+  }
+
+  render() {
+    return (
+      <AnimateHeight
+        animateOpacity
+        duration={500}
+        height={this.state.height}
+        aria-hidden={false}
+      >
+        <RenderBlocks {...this.props} />
+      </AnimateHeight>
+    );
+  }
+}
+
+const Edit = (props) => {
+  const {
+    data = {},
+    tabsList = [],
+    tabs = {},
+    activeTabIndex = 0,
+    setActiveTab = noop,
+    editingTab = null,
+    setEditingTab = noop,
+    id,
+  } = props;
+
+  const accordionConfig =
+    config.blocks.blocksConfig[TABS_BLOCK].templates?.['accordion'] || {};
+  const { icons, semanticIcon, transformWidth = 800 } = accordionConfig;
+
+  const tabsContainer = React.useRef();
+  const [mounted, setMounted] = React.useState(false);
+  const [hashTab, setHashTab] = React.useState(false);
+  const [initialWidth, setInitialWidth] = React.useState(transformWidth);
+
+  const schema = React.useMemo(
+    () =>
+      config.blocks.blocksConfig[TABS_BLOCK].templates?.['default']?.schema(
+        config,
+        props,
+      ) || {},
+    [props],
+  );
+
+  const getDataValue = React.useCallback(
+    (key) => {
+      return (
+        (schema.properties[key]?.value || data[key]) ??
+        schema.properties[key]?.defaultValue
+      );
+    },
+    [schema, data],
+  );
+  const items = tabsList.map((tab, index) => {
+    const title = tabs[tab]?.title;
+    const defaultTitle = `Tab ${index + 1}`;
+    const active = activeTabIndex === index;
+    const tabsDescription = data.description;
+    return {
+      title: (
+        <>
+          <MenuItem
+            {...props}
+            key={tab}
+            editingTab={editingTab}
+            index={index}
+            setEditingTab={setEditingTab}
+            tab={tab}
+            tabsTitle={title || defaultTitle}
+            tabsDescription={tabsDescription}
+          />
+        </>
+      ),
+      content: (
+        <Tab {...props} tab={tab} content={tabs[tab]} aria-hidden={false} />
+      ),
+      key: tab,
+      tabClassName: cx('ui button item title', { active }),
+      panelClassName: cx('ui bottom attached segment tab', {
+        active,
+      }),
+    };
+  });
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  React.useEffect(() => {
+    if (!mounted) return;
+    const { blockWidth, tabsTotalWidth } = tabsContainer.current?.state || {};
+    setInitialWidth(
+      tabsTotalWidth < blockWidth ? tabsTotalWidth + 1 : blockWidth + 1,
+    );
+  }, [mounted]);
+
+  useLayoutEffect(() => {
+    if (document.activeElement.role !== 'tab') return;
+    if (
+      document.getElementById(id)?.getElementsByClassName('tab active').length >
+      0
+    ) {
+      let activeTabDiv = document
+        .getElementById(id)
+        .getElementsByClassName('tab active')[0];
+      activeTabDiv.setAttribute('tabindex', '0');
+      activeTabDiv.setAttribute('className', 'accessibility-accordion-tab');
+      activeTabDiv.focus();
+    }
+  }, [activeTabIndex, id]);
+
+  return (
+    <div
+      tabIndex="0"
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          const focusedElement = document.activeElement;
+          if (focusedElement) focusedElement.click();
+        }
+      }}
+      role="tab"
+    >
+      <Tabs
+        ref={tabsContainer}
+        transformWidth={initialWidth}
+        selectedTabKey={tabsList[activeTabIndex]}
+        unmountOnExit={false}
+        items={items}
+        onChange={(tab) => {
+          const { blockWidth } = tabsContainer.current?.state || {};
+          const tabWithHash = getParentTabFromHash(
+            data,
+            props.location.hash.substring(1),
+          );
+          if (tabWithHash === tabsList[activeTabIndex] && !hashTab)
+            setHashTab(true);
+          else if (tab !== tabsList[activeTabIndex]) {
+            setActiveTab(tab);
+          } else if (blockWidth <= initialWidth) {
+            setActiveTab(null);
+          }
+        }}
+        tabsWrapperClass={cx(
+          props?.data?.accordionIconRight ? 'tabs-accordion-icon-right' : '',
+          'ui pointing secondary menu',
+          'tabs-accessibility',
+          data?.theme ? `theme-${data?.theme}` : '',
+          {
+            inverted: getDataValue('menuInverted'),
+          },
+        )}
+        showMore={false}
+      />
+    </div>
+  );
+};
+
+export default compose(withScrollToTarget, withRouter)(Edit);
