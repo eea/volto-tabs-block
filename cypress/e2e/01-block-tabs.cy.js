02b47d0ecabf4f8b84c7fc6b8ed8b2411b9e3965
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-09-15 14:14:01 +0300

    add test

--- cypress/e2e/01-block-tabs.cy.js
+++ cypress/e2e/01-block-tabs.cy.js
@@ -72,4 +72,56 @@ describe('Blocks Tests', () => {
     cy.get('.slick-arrow').click();
     cy.get('.block.image');
   });
+  it('Test accordion template', () => {
+    // Change page title
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('My Add-on Page');
+
+    cy.get('.documentFirstHeading').contains('My Add-on Page');
+
+    cy.getSlate().click();
+
+    // Add block
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Media').click();
+    cy.get('.content.active.media .button.image').contains('Image').click();
+
+    cy.getSlate().click();
+
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active.common .button.tabs_block')
+      .contains('Tabs')
+      .click({ force: true });
+
+    cy.get('.field-wrapper-title input').last().type('Tab 1');
+    cy.get('.field-wrapper-template #field-template').click();
+    cy.get('.react-select__menu').contains('Accordion').click();
+    cy.get('.field-wrapper-verticalAlign #field-verticalAlign').click();
+    cy.get('.react-select__menu').contains('Middle').click();
+
+    cy.get('.field-wrapper-menuAlign #field-menuAlign').click();
+    cy.get('.react-select__menu').contains('Left').click();
+    cy.get('.field-wrapper-menuPosition #field-menuPosition').first().click();
+    cy.get('.react-select__menu').contains('Top').click();
+
+    cy.get('.tabs-block [contenteditable=true]').first().type('Hydrogen');
+    cy.get('.tabs-block .ui.left.menu .item').last().click();
+    cy.get('.tabs-block').contains('Tab 2').click();
+    cy.get('.tabs-block.edit [contenteditable=true]').first().type('Oxygen');
+
+    // Save
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+
+    // then the page view should contain our changes
+    cy.contains('My Add-on Page');
+    cy.contains('Hydrogen');
+
+    cy.get('.tabs-block p').contains('Tab 2').click();
+    cy.contains('Oxygen');
+
+    cy.get('.slick-arrow').click();
+    cy.get('.block.image');
+  });
 });
