9a45825f80162f734872786fd0fe9359dd57ad9f
David Ichim <ichim.david@gmail.com>
2024-03-07 09:21:33 +0200

    fix(tests): after change from p tag to reference the .menu-item-text class
    - this way the tag can be anything as long as the class remains

--- cypress/e2e/01-block-tabs.cy.js
+++ cypress/e2e/01-block-tabs.cy.js
@@ -169,10 +169,14 @@ describe('Blocks Tests', () => {
     cy.get('.tabs-block .horizontal-responsive.tabs').should('exist');
     cy.contains('Horizontal First Item');
 
-    cy.get('.tabs-block p').contains('Tab 2').parent().focus().type('{enter}');
+    cy.get('.tabs-block .menu-item-text')
+      .contains('Tab 2')
+      .parent()
+      .focus()
+      .type('{enter}');
     cy.contains('Horizontal Second Item');
 
-    cy.get('.tabs-block p').contains('Tab 1').click();
+    cy.get('.tabs-block .menu-item-text').contains('Tab 1').click();
     cy.contains('Horizontal First Item');
   });
 
